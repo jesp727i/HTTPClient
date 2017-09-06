0a8f4927fc2fb49e9144ae165815b8ebc466f252
jespe <jespe@DESKTOP-RE9P93D>
2017-09-06 11:56:54 +0200

    Add project files.

---
+++ HTTPClient/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net.Sockets;
+
+namespace HTTPClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("server name");
+            string server = Console.ReadLine();
+            TcpClient client = new TcpClient(server, 80);
+            StreamReader sr = new StreamReader(client.GetStream());
+            StreamWriter sw = new StreamWriter(client.GetStream());
+
+            sw.WriteLine("GET /RegneWcfService.svc/RESTjson/Add?a=1&b=1 HTTP/1.1");
+            sw.WriteLine("Host: webservicedemo.datamatiker-skolen.dk\n\n");
+            sw.Flush();
+
+            int data = sr.Read();
+            while (data != -1)
+            {
+                char c = (char)data;
+                Console.Write(c);
+                data = sr.Read();
+            }
+            //string data = sr.ReadToEnd();
+            //while (data != null)
+            //{
+            //    Console.WriteLine("data");
+            //    data = sr.ReadLine();
+            //}
+            Console.ReadKey();
+            client.Close();
+        }
+    }
+}
